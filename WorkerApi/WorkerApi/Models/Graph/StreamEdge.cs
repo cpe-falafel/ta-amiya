e69f4b905c74806a03c87c261e58809268eeab99
Josse DE OLIVEIRA <contactme@webjosse.fr>
2025-01-06 10:10:31 +0100

    feat: Json to graph

---
+++ WorkerApi/WorkerApi/Models/Graph/StreamEdge.cs
@@ -0,0 +1,20 @@
+﻿using QuickGraph;
+
+namespace WorkerApi.Models.Graph
+{
+    public class StreamEdge : Edge<FilterVertex>
+    {
+
+        public String StreamName { get; }
+
+        public uint SrcOrder { get; }
+
+        public uint DstOrder { get; }
+
+
+        public StreamEdge(FilterVertex source, FilterVertex target, string name) : base(source, target)
+        {
+            StreamName = name;
+        }
+    }
+}
---
+++ WorkerApi/WorkerApi/Services/FilterVertexFactory.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using QuickGraph;
+using WorkerApi.Models.Filters;
+using WorkerApi.Models.Graph;
+
+namespace WorkerApi.Services
+{
+    class FilterVertexFactory
+    {
+        public static FilterVertex GenerateFilterVertex(string key, FilterGraphItem item) {
+            switch (item.Type)
+            {
+                case "drawbox":
+                    return new DrawboxFilter(key, item);
+                default:
+                    return null!;
+            }
+        }
+    }
+
+}
